b29f2139258dd1afc285d59d4fc9c96fa96e40da
shivang <shivangmathur101@gmail.com>
2022-02-01 17:12:28 +0530

    added reusable components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,21 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 3, 28);
+const ExpenseItem = (props) => {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
-      <div className="expense-item__description">
-        <h2>Car Insurance</h2>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
       </div>
-      <div className="expense-item__price">$295</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item__price">{props.amount}</div>
     </div>
   );
 };
